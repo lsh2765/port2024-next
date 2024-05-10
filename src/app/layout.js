b29199da5b568b94e1a77e28ee81bab241ec458b
Seonghyun Lim <lsh2765@naver.com>
2024-05-11 00:59:53 +0900

    셋팅완료

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "포트폴리오 사이트 만들기 😇 Next-Site",
+  description: "한가지 사이트를 다양한 프레임워크를 통해 만들어 보는 포트폴리오 사이트입니다.",
+  keywords: ["포트폴리오", "리액트", "뷰", "넥스트", "웹스토리보이", "portfolio", "react.js", "vue.js", "next.js", "webstoryboy"],
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <link rel="icon" type="image/svg+xml" href="assets/img/favicon.svg" />
+      <body>{children}</body>
+    </html>
+  );
+}
