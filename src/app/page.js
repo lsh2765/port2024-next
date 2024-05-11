41611ace9a0a4329cbd75cad98d1004a1814f8b3
Seonghyun Lim <lsh2765@naver.com>
2024-05-11 23:55:55 +0900

    헤더 완료

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,33 @@
-export default function Home () { 
+"use client";
+import React, { useEffect } from "react";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Intro from "@/components/Intro";
+import Skill from "@/components/Skill";
+import Port from "@/components/Port";
+import Site from "@/components/Site";
+import Contact from "@/components/Contact";
+import Skip from "@/components/Skip";
+import lenis from "@/utils/lenis";
+import link from "@/utils/link";
+
+export default function Home () {
+  useEffect(() => { 
+    lenis();
+    link();
+  }, []); 
   return ( 
-    <div>next.js</div>
-  )
+    <>
+        <Skip />
+        <Header />
+        <main id="main" role="main">
+            <Intro />
+            <Skill />
+            <Site />
+            <Port />
+            <Contact />
+        </main>
+        <Footer />
+    </>
+  );
 }
