05d28ac1874213c741fa8fdaed67db2a2dd73071
Seonghyun Lim <lsh2765@naver.com>
2024-05-12 09:14:53 +0900

    포트폴리오 완성

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
+import Image from "next/image"
+import { portText } from '@/constants'
+
+
 const Port = () => {
   return (
-    <div>Port</div>
+    <section id="port"> 
+        <div className="port__inner">
+            <div className="port__title">
+                portfolio <em>포폴 작업물</em>
+            </div>
+            <div className="port__wrap">
+              {portText.map((port, key) => ( 
+                <article className={`port__item p${key+1}}`} key={key}>
+                <span className="num">{port.num}.</span>
+                <a href={port.code} target="_blank" className="img">
+                    <Image src={port.img} alt={port.name} width={420} height={262}/>
+                </a>
+                <h3 className="title">{port.title}</h3>
+                <p className="desc">{port.desc}</p>
+                <a href={port.view} target="_blank" className="site">사이트 보기</a>
+            </article>
+              ))}     
+            </div>
+        </div>
+    </section>
   )
 }
 
